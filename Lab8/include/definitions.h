4b5d5d2d2888df84e2523e6d62d580e86eb9dcc4
joselumarja <34023329+joselumarja@users.noreply.github.com>
2019-05-06 20:33:01 +0200

    Practica 8

---
+++ Lab8/include/definitions.h
@@ -0,0 +1,22 @@
+#ifndef DEFINITIONS_H
+
+#define TRUE 1
+#define FALSE 0
+
+struct nearbyCarsState
+{
+    int ReadyOperation;
+    int CarsInProximities;
+};
+enum TrafficLightState
+{
+    WaitingInterrupt,
+    CarGreen,
+    ButtonPressed,
+    CarYellow,
+    CarRed,
+    CarStaticRed,
+    WalkerBlinkingGreen
+};
+
+#endif
